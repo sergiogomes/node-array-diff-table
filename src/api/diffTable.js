2cc98f3666ac774ed2a73e4b36bb61c86cbcac38
sergiogomes <sergiopgomes92@gmail.com>
2022-03-12 16:54:21 -0300

    feat: :sparkles: work on the table to render the differences

--- src/api/diffTable.js
+++ src/api/diffTable.js
@@ -1,8 +1,66 @@
 const express = require('express')
 const router = express.Router()
 
+const mockedHeaders = [
+  '_id',
+  'someKey',
+  'meta_subKey1',
+  'meta_subKey2',
+  'meta_subKey3',
+]
+
+const mockedData = [
+  {
+    _id: {
+      value: 1,
+      class: ''
+    },
+    someKey: {
+      value: 'HANGUP',
+      class: 'fw-bold'
+    },
+    meta_subKey1: {
+      value: 1234,
+      class: ''
+    },
+    meta_subKey2: {
+      value: 'DELETED',
+      class: 'fw-bold'
+    },
+    meta_subKey3: {
+      value: '',
+      class: ''
+    },
+  },
+  {
+    _id: {
+      value: 2,
+      class: 'fw-bold'
+    },
+    someKey: {
+      value: 'RINGING',
+      class: 'fw-bold'
+    },
+    meta_subKey1: {
+      value: 5678,
+      class: 'fw-bold'
+    },
+    meta_subKey2: {
+      value: 207,
+      class: 'fw-bold'
+    },
+    meta_subKey3: {
+      value: 52,
+      class: 'fw-bold'
+    },
+  }
+]
+
 router.get('/', (req, res) => {
-  res.render('diff-table')
+  res.render('diff-table', {
+    tableHeaders: mockedHeaders,
+    tableData: mockedData,
+  })
 })
 
 module.exports = router
