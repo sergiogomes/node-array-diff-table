79d7755babd58653f0cc8d18f5e9f3276794c8bc
sergiogomes <sergiopgomes92@gmail.com>
2022-03-12 15:57:23 -0300

    feat: :tada: create first api route

--- src/app.js
+++ src/app.js
@@ -3,9 +3,13 @@ const path = require('path')
 const app = express()
 const port = process.env.port || 3000
 
+const indexRouter = require('./api/index')
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
+
+app.use('/', indexRouter)
 
 app.listen(port, () => console.log(`Node Array Diff Table server listening on port ${port}`))
