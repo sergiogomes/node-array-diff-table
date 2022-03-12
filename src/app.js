b721e4992a496753dc0e5abd760f742f8c8627d7
sergiogomes <sergiopgomes92@gmail.com>
2022-03-12 16:21:32 -0300

    feat: :sparkles: create diff table api route

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app = express()
 const port = process.env.port || 3000
 
 const indexRouter = require('./api/index')
+const diffTableRouter = require('./api/diffTable')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -11,5 +12,6 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.use('/', indexRouter)
+app.use('/diff-table', diffTableRouter)
 
 app.listen(port, () => console.log(`Node Array Diff Table server listening on port ${port}`))
