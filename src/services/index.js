f4eb7818705b6b2be17477e6c36225565b8428e6
sergiogomes <sergiopgomes92@gmail.com>
2022-03-12 17:39:35 -0300

    feat: :sparkles: create recursive flat function for objects and arrays

---
+++ src/services/index.js
@@ -0,0 +1,234 @@
+const prevArray = [
+  {
+    id: "23b9dbff",
+    name: "Jessie",
+    age: 50,
+    children: [
+      {
+        id: "5c0f3094",
+        name: "Peter",
+        age: 20
+      },
+      {
+        id: "c1484221",
+        name: "Paul",
+        age: 32,
+        children: [
+          {
+            id: "2e6d866e",
+            name: "Carol",
+            age: 12
+          },
+          {
+            id: "e48a27ad",
+            name: "Hester",
+            age: 15
+          }
+        ]
+      },
+      {
+        id: "8a265c23",
+        name: "Hilda",
+        age: 25
+      }
+    ]
+  },
+  {
+    id: "53164b2b",
+    name: "Mathew",
+    age: 70,
+    children: [
+      {
+        id: "b14a960c",
+        name: "Spencer",
+        age: 45,
+        children: [
+          {
+            id: "ff3c260c",
+            name: "Joseph",
+            age: 22
+          },
+          {
+            id: "7c60920a",
+            name: "Robert",
+            age: 27,
+            children: [
+              {
+                id: "0e11874f",
+                name: "Ian",
+                age: 2
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: "5a4bdc98",
+    name: "Claire",
+    age: 63,
+    children: [
+      {
+        id: "014b62a3",
+        name: "Adrian",
+        age: 41
+      },
+      {
+        id: "a1899541",
+        name: "Julie",
+        age: 32,
+        children: [
+          {
+            id: "013362a3",
+            name: "Patricia",
+            age: 4
+          }
+        ]
+      }
+    ]
+  }
+];
+
+const currArray = [
+  {
+    id: "23b9dbff",
+    name: "Jessie",
+    age: 50,
+    children: [
+      {
+        id: "5c0f3094",
+        name: "Pedro",
+        age: 20
+      },
+      {
+        id: "c1484221",
+        name: "Paulo",
+        age: 32,
+        children: [
+          {
+            id: "2e6d866e",
+            name: "Carol",
+            age: 12
+          },
+          {
+            id: "e48a27ad",
+            name: "Hester",
+            age: 15
+          }
+        ]
+      },
+      {
+        id: "8a265c23",
+        name: "Hilda",
+        age: 25
+      }
+    ]
+  },
+  {
+    id: "53164b2b",
+    name: "Mateus",
+    age: 70,
+    children: [
+      {
+        id: "b14a960c",
+        name: "Spencer",
+        age: 45,
+        children: [
+          {
+            id: "ff3c260c",
+            name: "Jose",
+            age: 32
+          },
+          {
+            id: "7c60920a",
+            name: "Robert",
+            age: 27,
+            children: [
+              {
+                id: "0e11874f",
+                name: "Ian",
+                age: 2
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: "5a4bdc98",
+    name: "Claire",
+    age: 63,
+    children: [
+      {
+        id: "014b62a3",
+        name: "Adrian",
+        age: 45
+      },
+      {
+        id: "a1899541",
+        name: "Julie",
+        age: 32,
+        children: [
+          {
+            id: "013362a3",
+            name: "Patricia",
+            age: 41
+          }
+        ]
+      }
+    ]
+  }
+];
+
+const myFlatFunction = (ob) => {
+  let result = {};
+  
+  for (const i in ob) {
+    
+    if (Array.isArray(ob[i])) {
+      for (let k = 0; k < ob[i].length; k++) {
+        const temp = myFlatFunction(ob[i][k]);
+        for (const j in temp) {
+        
+          result[i + '.' + j] = temp[j];
+        }
+      }
+    }
+
+    else if ((typeof ob[i]) === 'object') {
+      const temp = myFlatFunction(ob[i]);
+      for (const j in temp) {
+        
+        result[i + '.' + j] = temp[j];
+      }
+    }
+    
+    else {
+      result[i] = ob[i];
+    }
+  }
+  return result;
+};
+
+const arrayDiffToHtmlTable = () => {
+  const flattenPrevArray = prevArray.reduce((acc, curr) => {
+    acc.push(myFlatFunction(curr))
+    return acc
+  }, [])
+  const flattenCurrArray = currArray.reduce((acc, curr) => {
+    acc.push(myFlatFunction(curr))
+    return acc
+  }, [])
+  console.log('flattenPrevArray', flattenPrevArray)
+  console.log('flattenCurrArray', flattenCurrArray)
+
+
+}
+
+
+
+module.exports = arrayDiffToHtmlTable
+
+arrayDiffToHtmlTable()
