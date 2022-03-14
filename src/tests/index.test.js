238f98d1c8809b1c9526cb44d7592fd8b06e90b3
sergiogomes <sergiopgomes92@gmail.com>
2022-03-13 21:13:00 -0300

    test: :test_tube: create first test

---
+++ src/tests/index.test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect
+
+describe('index service', () => {
+  describe('sum 1 + 1', () => {
+    it('should result in 2', () => {
+      expect(2).to.equal(2)
+    })
+  })
+})
